a416ebaa5a30fc56e88348aa4e7ecd2305f426d4
shreyaspapi <spapinwar@gmail.com>
2019-02-17 15:19:46 +0530

    updating docx.py

--- docx.py
+++ docx.py
@@ -8,9 +8,8 @@ document.add_heading('Document Title', 0)
 p = document.add_paragraph('A plain paragraph having some ')
 p.add_run('bold').bold = True
 p.add_run(' and some ')
-p.add_run('italic.').italic = True
 
-document.add_heading('Heading, level 1', level=1)
+document.add_heading('Example Heading', level=1)
 document.add_paragraph('Intense quote', style='Intense Quote')
 
 document.add_line_break()
